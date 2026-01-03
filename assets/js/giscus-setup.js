3711a26968fd9360caa8a7ebbe4914b579f38057
viktordremin <dremin_viktor@mail.ru>
2026-01-03 22:47:34 +0000

    Deploying to gh-pages from @ viktordremin/viktordremin.github.io@4dbed5dee371e6744a27d557127fc1a10cb7ec43 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ViktorDremin/ViktorDremin.github.io",
+    "data-repo": "viktordremin/viktordremin.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
