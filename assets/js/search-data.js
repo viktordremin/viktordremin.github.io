feaf67cd73023371f0794ae8eac7c2971b9fe7da
viktordremin <dremin_viktor@mail.ru>
2026-01-05 18:48:51 +0000

    Deploying to gh-pages from @ viktordremin/viktordremin.github.io@7bb474b8021bb86583a3ee27ad8be52dfdd62d23 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A collection of my key projects.",
+          description: "A collection of my key projects",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
