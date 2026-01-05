c69da8e1088c9910a8af4efc0f8285bdf540cd41
viktordremin <dremin_viktor@mail.ru>
2026-01-05 16:17:02 +0000

    Deploying to gh-pages from @ viktordremin/viktordremin.github.io@0d90fb364287d2ddd9a9499429ef57a7e1d47140 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -494,7 +494,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=sY9tLgsAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
